417a04c3ebc92ce04d72f8df96e43b62719cbf16
Meliza-fb <32288937+Meliza-fb@users.noreply.github.com>
2018-02-22 14:55:09 -0300

    Revert "Firebase"

--- assets/JS/app.js
+++ assets/JS/app.js
@@ -1,44 +1,72 @@
 $(document).ready(function() {
-  /* Api marvel*/
-  let inputText = document.getElementById('input-text');
-  let btnSearch = document.getElementById('btn-search');
-  let searchedForText;
+  /*Api marvel*/
+let inputText = document.getElementById('input-text');
+let btnSearch = document.getElementById('btn-search');
+let searchedForText;
 
+btnSearch.addEventListener('click',function(){
+	$('.marvelDate').empty();
+	$('.imgMarvel').empty();
+	fetch('http://gateway.marvel.com/v1/public/comics?apikey=a60bd159889749a73669c0be9f91ce67&ts=9&hash=45cbdbeb188c66926f8b050dde897f1b')
+  .then (function(response){
+    console.log(response);
+    return response.json();
+  })
+  .then(function(data){
+    console.log(data);
+    let content = [];
+  for(let i = 0; i < data.data.results.length; i++){
+     content.push(data.data.results[i].title);
+   }
+   console.log(content);
+   $('.marvelDate').append('<h5>' + content + '</h5>');
+ });
+});
 
-  /* btnSearch.addEventListener('click', function() {
-    $('.marvelDate').empty();
-    $('.imgMarvel').empty();
-    fetch('http://gateway.marvel.com/v1/public/comics?apikey=a60bd159889749a73669c0be9f91ce67&ts=9&hash=45cbdbeb188c66926f8b050dde897f1b')
-      .then(function(response) {
-        console.log(response);
-        return response.json();
-      })
-      .then(function(data) {
-        console.log(data);
-        let content = [];
-        for (let i = 0; i < data.data.results.length; i++) {
-          content.push(data.data.results[i].title);
-        }
-        console.log(content);
-        $('.marvelDate').append('<h5>' + content + '</h5>');
-      });
-  }); */
-  function buscar() {
-    $('.marvelDate').empty();
-    $('.imgMarvel').empty();
-    fetch('http://gateway.marvel.com/v1/public/comics?apikey=a60bd159889749a73669c0be9f91ce67&ts=9&hash=45cbdbeb188c66926f8b050dde897f1b')
-      .then(function(response) {
-        console.log(response);
-        return response.json();
-      })
-      .then(function(data) {
-        console.log(data);
-        let content = [];
-        for (let i = 0; i < data.data.results.length; i++) {
-          content.push(data.data.results[i].title);
-        }
-        console.log(content);
-        $('.marvelDate').append('<h5>' + content + '</h5>');
-      });
-  }
+   /* Firebase*/
+   // Ingreso usuarios o logueo
+   function ingresar() {
+     console.log('diste click en Ingresar');
+     var email = document.getElementById('email').value;
+     var password = document.getElementById('password').value;
+     // console.log(email);
+     // console.log(password);
+     if (email === '' || password === '') {
+       alert('Debe Ingresar datos');
+     } else {
+       firebase.auth().signInWithEmailAndPassword(email, password)
+         .catch(function(error) {
+           // Handle Errors here.
+           var errorCode = error.code;
+           var errorMessage = error.message;
+           console.log(errorCode);
+           console.log(errorMessage);
+         // ...
+         });
+     }
+   }
+   // Registro de nuevos usuarios*/
+   function registrar() {
+   // console.log('diste click en Ingresar');
+     var email = document.getElementById('email2').value;
+     var password = document.getElementById('password2').value;
+     // console.log(email);
+     // console.log(password);
+     if (email === '' || password === '') {
+       alert('Debe Ingresar datos');
+     } else {
+       firebase.auth().createUserWithEmailAndPassword(email, password)
+         .then(function() {
+           // verificarEmail()
+         })
+         .catch(function(error) { // promesa catch, si la autentificacion no ocurre catch ejecuta una funcion con parametro e, donde e guardo 2 errores en variables
+           // Handle Errors here.
+           var errorCode = error.code;
+           var errorMessage = error.message;
+           console.log(errorCode);
+           console.log(errorMessage);
+           // ...
+         });
+       }
+     }
 });
