a42a067a8b505a288368533a5c33eb4ed8985269
Meliza-fb <maite.e.figue@hotmail.com>
2018-02-22 14:36:17 -0300

    cambios con Api

--- assets/JS/app.js
+++ assets/JS/app.js
@@ -8,20 +8,46 @@ btnSearch.addEventListener('click',function(){
 	$('.marvelDate').empty();
 	$('.imgMarvel').empty();
 	fetch('http://gateway.marvel.com/v1/public/comics?apikey=a60bd159889749a73669c0be9f91ce67&ts=9&hash=45cbdbeb188c66926f8b050dde897f1b')
-  .then (function(response){
+  .then(function(response){
     console.log(response);
     return response.json();
   })
   .then(function(data){
-    console.log(data);
+    console.log(data.data.results);
     let content = [];
-  for(let i = 0; i < data.data.results.length; i++){
-     content.push(data.data.results[i].title);
-   }
-   console.log(content);
-   $('.marvelDate').append('<h5>' + content + '</h5>');
+    for(let i = 0; i < marvelDescription.length; i++){//cuento mi database
+      content.push(marvelDescription[i].title);//rescato los title de mki database
+  }
+  $('#marvelDate').append('<ol><li>' + content + '</li></ol>');
+  /*$('.imgMarvel').append('<img src="http://i.annihil.us/u/prod/marvel/i/mg/1/e0/4bb4ecb6aa5a9'  + '.jpg">')*/
  });
-});
+ });
+/*
+   let img = document.getElementById('imgMarvel');//llamo a mi contenedor de imagenes
+   for (let j = 0; j < data.data.results.length; j++) {//recorro la API
+     let title= data.data.results[j].title;//rescato los title de API
+     console.log(content[i]);
+     if (title === content[i]) { //comparo la title de API con mi data base
+
+      // let BoxContainer= document.getElementById('container-box');//llamo a mi caja contenedora
+      // BoxContainer.appendChild(img);//agrego mi  content de imagenes a mi BoxContainer
+       //img.appendChild(marvelDescription[j].image);//a img le agrego mi database buscando imagenes
+console.log(marvelDescription[i].image);
+       $('#container-box').append('<img src='+marvelDescription[i].image+'>');
+       /*let textContainer = document.getElementById('container-text');//llamo a mi caja contenedora
+       textContainer.append(content[j]);//le agrego a mi caja contenedora los title de la Api
+       content.appendChild(content[j].title);//?????*/
+
+
+
+   /*
+   for (let i = 0; i < data.data.results[0].thumbnail.length; i++) {
+     characters.push(data.data.results[0].thumbnail[i].path);
+   }*/
+
+
+
+
 
    /* Firebase*/
    // Ingreso usuarios o logueo
--- assets/JS/appModal.js
+++ assets/JS/appModal.js
@@ -2,3 +2,90 @@ $(document).ready(function() {
   // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
   $('.modal').modal();
 });
+
+const marvelDescription= [
+  {
+  "title": "Cap Transport (2005) #18",
+  "image":"assets/IMG/data/Cap_Transport.jpg"
+},
+{
+  "title": "Gun Theory (2003) #4",
+  "image":"assets/IMG/data/Gun_Theory.jpg"
+},
+{
+  "title": "Ultimate X-Men (Spanish Language Edition) (2000) #8",
+  "image":"assets/IMG/data/Ultimate_X-Men.jpg"
+},
+{
+  "title": "Ultimate X-Men (Spanish Language Edition) (2000) #8",
+  "image":"assets/IMG/data/Ultimate_X-Men.jpg"
+},
+{
+"title": "Squadron Supreme (2015) #9 (Frison Mighty Men Variant)",
+"image":"assets/IMG/data/Squadron_Supreme.jpg"
+},
+{
+  "title": "Marvels Vol. 1 (1994) #1",
+  "image":"assets/IMG/data/Marvels_Vol_1.jpg"
+},
+{
+  "title": "Universe X: Cap (2001)",
+  "image":"assets/IMG/data/Universe_X.jpg"
+},
+{
+  "title": "Amazing Spider-Man (1999) #558 (Turner Variant)",
+  "image":"assets/IMG/data/amazing_spiderman.jpg"
+},
+{
+  "title": "The Astonishing Ant-Man (2015) #10 (Bagley Mighty Men Variant)",
+  "image":"assets/IMG/data/The_Astonishing_Ant-Man.jpg"
+},
+{
+  "title": "Cap Transport (2005) #8",
+  "image":"assets/IMG/data/transport.jpg"
+},
+{
+  "title": "Halo Chronicles (2009) #2",
+  "image":"assets/IMG/data/Halo_Chronicles.jpg"
+},
+{
+  "title": "Ultimate X-Men (Spanish Language Edition) (2000) #10",
+  "image":"assets/IMG/data/Ultimate_X-Men-2.jpg"
+},
+{
+  "title": "Ultimate Spider-Man (Spanish Language Edition) (2000) #3",
+  "image":"assets/IMG/data/Ultimate_Spider-Man.jpg"
+},
+{
+  "title": "Ultimate Spider-Man Ultimate Collection Book 1 (Trade Paperback)",
+  "image":"assets/IMG/data/Spider-Man_Ultimate_Collection.jpg"
+},
+{
+  "title": "Magician: Apprentice Riftwar Saga (2010) #15",
+  "image":"assets/IMG/data/Magician_Apprentice_Riftwar.jpg"
+},
+{
+"title": "Official Marvel Universe Handbook (2009) #2",
+"image":"assets/IMG/data/Marvel_Universe_Handbook.jpg"
+},
+{
+  "title": "Cap Transport (2005) #11",
+  "image":"assets/IMG/data/Cap_Transport.jpg"
+},
+{
+  "title": "Wolverine Saga (2009) #7",
+  "image":"assets/IMG/data/Wolverine_Saga.jpg"
+},
+{
+  "title": "Cap Transport (2005) #2",
+  "image":"assets/IMG/data/Cap_2_Transport.jpg"
+},
+{
+  "title": "Gun Theory (2003) #3",
+  "image":"assets/IMG/data/Gun_Theory_2.jpg"
+},
+{
+  "title": "Marvels Vol. 1 (1994) #4",
+  "image":"assets/IMG/data/Marvels_Vol_1.jpg"
+}
+];
